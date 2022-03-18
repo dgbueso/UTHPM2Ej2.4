168eae28bd10c99743496824f640d613db34abb9
danie <danie@DANIEL-GB>
2022-03-18 15:04:39 -0600

    Add project files.

---
+++ PhotoApp/PhotoApp/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PhotoApp.Views;
+using PhotoApp.Controllers;
+using System.IO;
+
+namespace PhotoApp
+{
+    public partial class App : Application
+    {
+        static Database database;
+        public static Database BaseDatos
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "PM02VideoApp.db3"));
+                }
+
+                return database;
+            }
+
+
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
